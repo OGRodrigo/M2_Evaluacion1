c064fc796dfef46157e18fa9ab38ab24b1f23787
O.G.Rodrigo <Darkscencia@gmail.com>
2026-09-06 16:48:51 -0300

    feat: agregar datos simulados de productos

---
+++ src/data/products.js
@@ -0,0 +1,56 @@
+export const games = [
+  {
+    id: 1,
+    name: "Elden Ring",
+    price: 39990,
+    category: "RPG",
+    platform: "PC / PS5",
+    image: "/images/elden.jpg",
+    rating: 4.9
+  },
+  {
+    id: 2,
+    name: "Cyberpunk 2077",
+    price: 29990,
+    category: "RPG",
+    platform: "PC / Xbox",
+    image: "/images/cyberpunk.jpg",
+    rating: 4.7
+  },
+  {
+    id: 3,
+    name: "Forza Horizon 5",
+    price: 34990,
+    category: "Carreras",
+    platform: "PC / Xbox",
+    image: "/images/forza.webp",
+    rating: 4.8
+  },
+  {
+    id: 4,
+    name: "Hades",
+    price: 14990,
+    category: "Acción",
+    platform: "PC / Switch",
+    image: "/images/hades.jpg",
+    rating: 4.8
+  },
+  {
+    id: 5,
+    name: "The Legend of Zelda",
+    price: 49990,
+    category: "Aventura",
+    platform: "Nintendo Switch",
+    image: "/images/zelda.jpg",
+    rating: 4.9
+  },
+  {
+    id: 6,
+    name: "Street Fighter 6",
+    price: 37990,
+    category: "Lucha",
+    platform: "PC / PS5",
+    image: "/images/street.jfif",
+    rating: 4.6
+  }
+];
